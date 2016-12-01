e93ca7ab70c9e4a2eae7db8d9582500e721cfa04
Zhiwei Han <hanzw356255531@icloud.com>
2016-12-01 16:39:55 +0100

    Error Plot finished

--- Makefile
+++ Makefile
@@ -1,4 +1,7 @@
-make: Makefile main.py
-	rm -f Plots/*y
+make: Makefile main.py clean
+	rm -f Plots/*y *.jpg
 	python main.py
 	rm -f Toolbox/*.pyc
+
+clean:
+	rm -f *.jpg
--- Toolbox/PlotAgent.py
+++ Toolbox/PlotAgent.py
@@ -1,3 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
 class PlotAgent(object):
     """docstring for PlotAgent"""
 
@@ -16,4 +20,17 @@ class PlotAgent(object):
         error_sets,
         plot_labels
     ):
-        pass
+        plt.figure(figsize=(15.0, 15.0))
+        for num in xrange(0, len(error_sets)):
+
+            t = np.arange(len(error_sets[num]))
+            image_index = num + 1
+            plt.subplot(4, 3, image_index)
+            plt.plot(t, error_sets[num])
+            plt.title(plot_labels[num])
+
+        plt.savefig('ErrorPlot.jpg')
+
+
+if __name__ == '__main__':
+    print int("25")
